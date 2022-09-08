591010379ad99e2c0393d430de80cb0579a97ec3
seunbabara-creator <onamadeseun303@gmail.com>
2022-09-08 14:29:48 -0700

    err

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Sucess)
+*/
+
+int main(void)
+{
+fwrite(STDOUT_FILENO,   "and that piece art is useful\"- Dora Korpar, 2015-10-19\n",59);
+return (1);
