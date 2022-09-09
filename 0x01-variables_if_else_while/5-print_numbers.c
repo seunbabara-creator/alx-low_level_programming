a4e66d0110be8c1287805090abcc8f70f99abaf1
seunbabara-creator <onamadeseun303@gmail.com>
2022-09-09 12:34:32 -0700

    printing 1-10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d\n", a);
+		return (0);
+	}
+}
