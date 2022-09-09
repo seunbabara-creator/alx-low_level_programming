d18e73549806bf59b14abad3a35aece1339c5a1e
seunbabara-creator <onamadeseun303@gmail.com>
2022-09-09 08:43:02 -0700

    fourth

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry poit
+ *
+ * Retun: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char twice;
+
+	for(twice = 'a'; twice <= 'z'; twice++)
+		putchar(twice);
+	for(twice = 'A'; twice <= 'Z'; twice++)
+		putchar(twice);
+	putchar('\n');
+	return (0);
+}
+
