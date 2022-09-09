1e472281437e27237165128988e0ab5e246959b3
seunbabara-creator <onamadeseun303@gmail.com>
2022-09-09 13:05:52 -0700

    Printing in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int d;
+	char new;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	for (new ='a'; new <= 'f'; new++)
+		putchar(new);
+	putchar('\n');
+	return (0);
+}
