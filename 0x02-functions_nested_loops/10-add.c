d5641666260df04e0890a0e20793c8f2a0d774d2
seunbabara-creator <onamadeseun303@gmail.com>
2022-09-14 03:42:39 -0700

    all the rest

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - check code
+ * @a: An integer a
+ * @b: An integer b
+ *
+ * Return: result od addition of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
